3ffa6d747ae0089f3c63c9cc05dbf5b7694d1b88
Nguyễn Khoa Hoàng <blcm2486@gmail.com>
2023-06-11 00:09:46 +0700

    feat: Test comments in HomePage

--- src/page/Home/Home.js
+++ src/page/Home/Home.js
@@ -12,8 +12,25 @@ import {
     faShower,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { CommentSection } from 'react-comments-section'
+import React, { useState } from 'react'
+import 'react-comments-section/dist/index.css'
 
 const Home = () => {
+    const [comments, setComments] = useState([{
+        userId: '02b',
+        comId: '017',
+        fullName: 'Lily',
+        userProfile: 'https://www.linkedin.com/in/riya-negi-8879631a9/',
+        text: 'I think you have a point🤔',
+        avatarUrl: 'https://ui-avatars.com/api/name=Lily&background=random',
+        replies: []
+    }]);
+
+    const HandleSubmitComment = () => {
+        console.log(comments);
+    };
+
     return (
         <div>
             <Header />
@@ -207,6 +224,26 @@ const Home = () => {
                     </div>
                 </section> */}
             </div>
+
+            {/* <CommentSection
+                currentUser={{
+                    currentUserId: '01a',
+                    currentUserImg:
+                        'https://ui-avatars.com/api/name=Riya&background=random',
+                    currentUserProfile:
+                        'https://www.linkedin.com/in/riya-negi-8879631a9/',
+                    currentUserFullName: 'Riya Negi'
+                }}
+                logIn={{
+                    loginLink: 'http://localhost:3001/',
+                    signupLink: 'http://localhost:3001/'
+                }}
+                commentData={comments}
+                onSubmitAction={(data) => console.log('check submit, ', data)}
+                // currentData={(data) => {
+                //     console.log('curent data', data)
+                // }}
+            /> */}
             <Footer />
         </div>
     );
