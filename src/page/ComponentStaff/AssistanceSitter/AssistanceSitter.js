f4db7b8d8f2106022493ddf6e9bc1aa10fbcfa01
caominhtri112201 <caominhtri112201@gmail.com>
2023-06-17 14:48:27 +0700

    feat/PM-23: Update chatbot

--- src/page/ComponentStaff/AssistanceSitter/AssistanceSitter.js
+++ src/page/ComponentStaff/AssistanceSitter/AssistanceSitter.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "./AssistanceSitter.css";
+import SitterService from "../../../config/service/SitterService";
 
 const AssistanceStaff = () => {
+    const [messages, setMessages] = useState([
+        { text: "Hello!", sender: "bot" },
+    ]);
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        const messageInput = event.target.elements.message;
+        const messageText = messageInput.value;
+
+        if (!messageText) {
+            return;
+        }
+
+        const newMessage = { text: messageText, sender: "user" };
+        setMessages((prevMessages) => [...prevMessages, newMessage]);
+
+        const botMessage = await sendMessage(messageText);
+        setMessages((prevMessages) => [
+            ...prevMessages,
+            { text: botMessage, sender: "bot" },
+        ]);
+
+        messageInput.value = "";
+    };
+
+    const sendMessage = async (messageText) => {
+        const response = await SitterService.chatBot(messageText);
+        return response.choices[0].message.content;
+    };
+
     return (
         <div className="home">
             <div className="container">
@@ -10,17 +42,24 @@ const AssistanceStaff = () => {
                         <h1>Chat</h1>
                     </div>
                     <div className="messages">
-                        <div className="message bot-message">
-                            <p>Hello!</p>
-                        </div>
-                        <div className="message user-message">
-                            <p>Hi!</p>
-                        </div>
+                        {messages.map((message, index) => (
+                            <div
+                                key={index}
+                                className={`message ${
+                                    message.sender === "bot"
+                                        ? "bot-message"
+                                        : "user-message"
+                                }`}
+                            >
+                                <p>{message.text}</p>
+                            </div>
+                        ))}
                     </div>
-                    <form className="message-form">
+                    <form className="message-form" onSubmit={handleSubmit}>
                         <input
                             type="text"
                             placeholder="Type your message here..."
+                            name="message"
                         />
                         <button type="submit">Send</button>
                     </form>
